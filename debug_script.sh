71e8a67a81774a6c0f0cc98f12f1c714dd9efd6e
BrianBTB <bballing1@gmail.com>
2014-10-29 17:33:51 -0400

    fixed

--- debug_script.sh
+++ debug_script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Extremely simple script made by SMOKE to aid in memtools testing
-git clone https://github.com/BrianBTB/memtools_vita
-cd memtools_vita
-git pull origin
 git fetch
-git checkout debug
+git checout debug
+git pull origin debug
 chmod a+x serv.py
 # Starting logfile
-script serv.py.log && ./serv.py
+script serv.py.log
