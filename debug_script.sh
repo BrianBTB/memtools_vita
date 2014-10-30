6229f5e3ddbbdf0dae3db8744fa288461c13c81f
BrianBTB <bballing1@gmail.com>
2014-10-29 18:16:58 -0700

    edits

--- debug_script.sh
+++ debug_script.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "Extremely simple script made by SMOKE to aid in memtools testing"
-echo "Type python serv.py after script finishes, then type exit after closing server to save log"
 git fetch
 git checkout debug
 git pull origin debug
 chmod a+x serv.py
 # Starting logfile
-echo "python serv.py && exit" | script serv.py.log
-exit
+script serv.py.log
